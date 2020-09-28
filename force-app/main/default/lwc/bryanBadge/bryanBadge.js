152c24ce2ceea545f3593bea85fb680a01fc8678
maroonsand <maroonsand@gmail.com>
2020-09-27 21:32:23 -0400

    updating bryanBadge js

--- force-app/main/default/lwc/bryanBadge/bryanBadge.js
+++ force-app/main/default/lwc/bryanBadge/bryanBadge.js
@@ -3,17 +3,12 @@ import insertBryan from '@salesforce/apex/BryanBadge.insertBryan';
 
 export default class bryansMethod extends LightningElement {
 
-    firstNameInput;
-    lastNameInput;
+   @track firstNameInput;
+   @track lastNameInput;
 
-    handleChange(event) {
-       const field = event.target.name;
-        if (field === 'firstName') {
-            this.firstNameInput = event.target.value;
-        } else if (field === 'lastName') {
-            this.lastNameInput = event.target.value;
-        }
-        console.log(event.target.value);
+    handleChange(event) 
+    {
+        this[event.target.name] = event.target.value;
     }
 
     buttonClicked()
